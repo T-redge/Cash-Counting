2571040701cfaf9a72f1e6be7d087a391c70b59a
T-Redge <tredgett.a@outlook.com>
2025-08-18 01:48:02 +0800

    save

--- src/monthly_takings/takings_input.rs
+++ src/monthly_takings/takings_input.rs
@@ -1,4 +1,7 @@
-use eframe::egui;
+use eframe::egui::{
+    self,
+    text::{CCursor, CCursorRange},
+};
 
 use crate::monthly_takings::{Date, create_label, date_picker::Day};
 
@@ -11,7 +14,7 @@ impl TakingsField {
     pub fn new() -> Self {
         Self {
             takings: 0.0,
-            text: String::from("0"),
+            text: "0".to_owned(),
             text_highlighted: false,
         }
     }
@@ -27,11 +30,36 @@ impl TakingsField {
             });
             ui_3.vertical_centered(|ui_3| {
                 ui_3.label(create_label("Takings").underline());
+                self.takings_textbox(ui_3);
             });
         });
-        ui.add_sized(
-            [400.0, 25.0],
-            egui::Button::new(create_label("Confirm Takings")),
-        );
+        if ui
+            .add_sized(
+                [400.0, 25.0],
+                egui::Button::new(create_label("Confirm Takings")),
+            )
+            .clicked()
+        {
+            self.takings = self.text.trim().parse::<f32>().unwrap();
+            self.text = "0".to_owned();
+        }
+    }
+    fn takings_textbox(&mut self, ui: &mut egui::Ui) {
+        let mut text = egui::TextEdit::singleline(&mut self.text)
+            .text_color(egui::Color32::WHITE)
+            .background_color(egui::Color32::BLACK)
+            .show(ui);
+        if !self.text_highlighted {
+            text.state.cursor.set_char_range(Some(CCursorRange::two(
+                CCursor::new(0),
+                CCursor::new(self.text.len()),
+            )));
+            text.state.store(ui.ctx(), text.response.id);
+            self.text_highlighted = true;
+        }
+
+        if text.response.gained_focus() {
+            self.text_highlighted = false;
+        }
     }
 }
